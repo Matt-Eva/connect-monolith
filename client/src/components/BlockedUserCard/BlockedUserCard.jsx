b513b1a84e05eb879bff8062a0bbbbb212031a9d
Matt Eva <matteweva@gmail.com>
2024-01-19 13:29:12 -0800

    added unblock and image icon to block user card

--- client/src/components/BlockedUserCard/BlockedUserCard.jsx
+++ client/src/components/BlockedUserCard/BlockedUserCard.jsx
@@ -1,11 +1,36 @@
 import { Link } from "react-router-dom"
+import CardImageIcon from "../CardImageIcon/CardImageIcon"
 import styles from "./BlockedUserCard.module.css"
 
 function BlockedUserCard({name, profileImg, uId}) {
+
+  const iconUser = [{
+    firstName: name,
+    profileImg
+  }]
+
+  const unblock = async () => {
+    try {
+        const res = await fetch(`/api/unblock-user/${uId}`, {
+            method: "DELETE",
+            credentials: "include"
+        })
+        if (res.ok){
+            setProfile({
+                ...profile,
+                blocked: false
+            })
+        }
+    } catch (e) {
+        console.error(e)
+    }
+}
+
   return (
-    <article className={`userCard ${styles.card}`}>
-        <img src={profileImg} alt={`${name} profile image`} className={`userCardImage`}/>
-        <p className={`userCardName`}>{name}</p>
+    <article className={`${styles.card}`}>
+        <CardImageIcon users={iconUser} />
+        <p className={styles.name}>{name}</p>
+        <button onClick={unblock}>unblock</button>
         <Link to={`/profile/${uId}`} className={`buttonLink`}>view profile</Link>
     </article>
   )
