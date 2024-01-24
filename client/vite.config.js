5f2a1ccbd57e0486712038a0f2edd1fbb68e8fa8
Matt Eva <matteweva@gmail.com>
2024-01-24 09:36:19 -0800

    set up basica pwa functionality without vite-pwa-plugin

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,52 +5,53 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-      },
-      manifest: {
-        name: "Connect",
-        short_name: "Connect",
-        description: "A social media and real time chat application.",
-        theme_color: "#ffffff",
-        display: "minimal-ui",
-        icons: [
-          {
-            "src": "pwa-64x64.png",
-            "sizes": "64x64",
-            "type": "image/png"
-          },
-          {
-            "src": "android-chrome-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png"
-          },
-          {
-            "src": "android-chrome-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png"
-          },
-          {
-            "src": "android-chrome-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "any"
-          },
-          {
-            "src": "maskable-icon-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "maskable"
-          }
-        ]
-      }
-    })
+    react()
+    //,
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   devOptions: {
+    //     enabled: true
+    //   },
+    //   workbox: {
+    //     globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+    //   },
+    //   manifest: {
+    //     name: "Connect",
+    //     short_name: "Connect",
+    //     description: "A social media and real time chat application.",
+    //     theme_color: "#ffffff",
+    //     display: "minimal-ui",
+    //     icons: [
+    //       {
+    //         "src": "pwa-64x64.png",
+    //         "sizes": "64x64",
+    //         "type": "image/png"
+    //       },
+    //       {
+    //         "src": "android-chrome-192x192.png",
+    //         "sizes": "192x192",
+    //         "type": "image/png"
+    //       },
+    //       {
+    //         "src": "android-chrome-512x512.png",
+    //         "sizes": "512x512",
+    //         "type": "image/png"
+    //       },
+    //       {
+    //         "src": "android-chrome-512x512.png",
+    //         "sizes": "512x512",
+    //         "type": "image/png",
+    //         "purpose": "any"
+    //       },
+    //       {
+    //         "src": "maskable-icon-512x512.png",
+    //         "sizes": "512x512",
+    //         "type": "image/png",
+    //         "purpose": "maskable"
+    //       }
+    //     ]
+    //   }
+    // })
   ],
   server :{
     proxy: {
