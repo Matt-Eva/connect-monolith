350f78ed66fa74803c4095e85149bd5c3cead849
Matt Eva <matteweva@gmail.com>
2024-02-02 11:24:32 -0800

    saving revert to previous state

--- client/src/root/Root.jsx
+++ client/src/root/Root.jsx
@@ -1,139 +1,74 @@
-import { useState, useEffect } from 'react'
-import { Outlet, Navigate, useNavigate, useLocation } from 'react-router-dom'
-import Header from '../components/Header/Header'
-import MainNavBar from "../components/MainNavBar/MainNavBar"
-import styles from './Root.module.css'
+import { useState, useEffect } from "react";
+import { Outlet, Navigate, useNavigate, useLocation } from "react-router-dom";
+import Header from "../components/Header/Header";
+import MainNavBar from "../components/MainNavBar/MainNavBar";
+import styles from "./Root.module.css";
 
 function Root() {
-  const [user, setUser] = useState(false)
-  const [loading, setLoading] = useState(true)
-  const [offlineDisplay, setOfflineDisplay] = useState(false)
-  const navigate = useNavigate()
-  const location = useLocation()
-  const [startingPath, setStartingPath] = useState(location.pathname)
+  const [user, setUser] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [offlineDisplay, setOfflineDisplay] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [startingPath, setStartingPath] = useState(location.pathname);
 
-  useEffect(() =>{
-    const getMe = async () =>{
+  useEffect(() => {
+    const getMe = async () => {
       try {
-        const res = await fetch("/api/me")
-        if(res.ok){
-          const data = await res.json()
-          setUser(data)
-          setLoading(false)
-        } else if (res.status === 401){
-          setUser(false)
-          setLoading(false)
-          navigate('/login')
+        const res = await fetch("/api/me");
+        if (res.ok) {
+          const data = await res.json();
+          setUser(data);
+          setLoading(false);
+        } else if (res.status === 401) {
+          setUser(false);
+          setLoading(false);
+          navigate("/login");
         } else {
-          throw new Error('Problem with network response')
+          throw new Error("Problem with network response");
         }
-      } catch (e){
-        setOfflineDisplay(true)
-        console.log("caught error")
-        console.error(e)
+      } catch (e) {
+        setOfflineDisplay(true);
+        console.log("caught error");
+        console.error(e);
       }
-    }
-    getMe()
-  }, [])
-
-<<<<<<< HEAD
+    };
     getMe();
   }, []);
 
-  useEffect(() => {
-    const hideNotifications = async () => {
-      if ("serviceWorker" in navigator) {
-        await navigator.serviceWorker.ready;
-        if (navigator.serviceWorker.controller) {
-          navigator.serviceWorker.controller.postMessage({
-            type: "focusState",
-            isFocused: true,
-          });
-        }
-      }
-    };
-    hideNotifications();
-  }, []);
-
-  useEffect(() => {
-    const handleVisibilityChange = async () => {
-      console.log("visibility changing");
-      if ("serviceWorker" in navigator) {
-        console.log("in visibility change");
-        if (!document.hidden) {
-          console.log("document is visible");
-          navigator.serviceWorker.ready
-            .then((reg) => {
-              reg.getNotifications().then((notifications) => {
-                for (let i = 0; i < notifications.length; i += 1) {
-                  notifications[i].close();
-                }
-              });
-            })
-            .then(() => {
-              if (navigator.serviceWorker.controller) {
-                navigator.serviceWorker.controller.postMessage({
-                  type: "focusState",
-                  isFocused: true,
-                });
-              }
-            });
-        } else {
-          console.log("document is hidden");
-          await navigator.serviceWorker.ready;
-          if (navigator.serviceWorker.controller) {
-            navigator.serviceWorker.controller.postMessage({
-              type: "focusState",
-              isFocused: false,
-            });
-          }
-        }
-      }
-    };
-
-    document.addEventListener("visibilitychange", handleVisibilityChange);
-
-    return () => {
-      document.removeEventListener("visibilitychange", handleVisibilityChange);
-    };
-  }, []);
-
   const login = async (email, password) => {
-=======
-  const login = async (email, password) =>{
->>>>>>> parent of 3cb9500 (set up frontend to hide notifications upon application open and application focus)
     const res = await fetch("/api" + "/login", {
-        method: "POST",
-        credentials: "include",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({email: email, password: password})
-    })
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: email, password: password }),
+    });
 
-    if (res.ok){
-      const data = await res.json()
-      setUser(data)
-      if (startingPath === "/login" || startingPath === "/new-account"){
-        navigate("/")
+    if (res.ok) {
+      const data = await res.json();
+      setUser(data);
+      if (startingPath === "/login" || startingPath === "/new-account") {
+        navigate("/");
       } else {
-        navigate(startingPath)
+        navigate(startingPath);
       }
     } else {
-      const error = await res.json()
-      console.error(error)
+      const error = await res.json();
+      console.error(error);
     }
-  }
+  };
 
   const logout = async () => {
     await fetch("/api" + "/logout", {
       method: "DELETE",
-      credentials: "include"
-    })
-    setStartingPath(location.pathname)
-    setUser(false)
-    navigate("/login")
-  } 
+      credentials: "include",
+    });
+    setStartingPath(location.pathname);
+    setUser(false);
+    navigate("/login");
+  };
 
   const createAccount = async (newUser) => {
     try {
@@ -141,45 +76,51 @@ function Root() {
         method: "POST",
         credentials: "include",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(newUser)
-      })
-      if(res.ok){
-        const data = await res.json()
-        setUser(data)
-        if (startingPath === "/login" || startingPath === "/new-account"){
-          navigate("/")
+        body: JSON.stringify(newUser),
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setUser(data);
+        if (startingPath === "/login" || startingPath === "/new-account") {
+          navigate("/");
         } else {
-          navigate(startingPath)
+          navigate(startingPath);
         }
       } else {
-        const error = await res.json()
+        const error = await res.json();
         // throw new Error(error.error)
       }
-    } catch(e) {
+    } catch (e) {
       // console.log(e)
-      throw new Error(e.message)
+      throw new Error(e.message);
     }
-  }
+  };
 
-  const destroyUser = () => setUser(false)
+  const destroyUser = () => setUser(false);
 
-  const outletContext = {user, login: login, logout, destroyUser, createAccount: createAccount}
+  const outletContext = {
+    user,
+    login: login,
+    logout,
+    destroyUser,
+    createAccount: createAccount,
+  };
 
-  if (loading && !offlineDisplay){
-    return <h1>Loading...</h1>
+  if (loading && !offlineDisplay) {
+    return <h1>Loading...</h1>;
   } else if (offlineDisplay) {
-    return <h1>You are currently offline</h1>
+    return <h1>You are currently offline</h1>;
   }
 
   return (
     <div className={styles.root}>
       {user ? <Header logout={logout} /> : <Navigate to="/login" />}
-      <Outlet context={outletContext}/>
+      <Outlet context={outletContext} />
       {user ? <MainNavBar /> : null}
     </div>
-  )
+  );
 }
 
-export default Root
+export default Root;
