f9a297c12506cf29b533f740573a5502142da3ad
Matt Eva <matteweva@gmail.com>
2024-02-06 19:03:38 -0800

    removing unecessary import

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,18 +1,27 @@
-import MainNavBar from "../MainNavBar/MainNavBar"
-import { Link } from "react-router-dom"
-import styles from "./Header.module.css"
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
 
 function Header() {
-
   return (
     <header className={styles.header}>
-        <h1 className={styles.title}> <Link to="/">CONNECT</Link></h1>
-        <Link to="/about" className={styles.about}>about</Link>
-        <Link to="/donate" className={styles.donate}>donate</Link>
-        <Link to="/get-app" className={styles.getApp}>get the app</Link>
-        <Link to="/account" className={styles.account}>account</Link>
+      <h1 className={styles.title}>
+        {" "}
+        <Link to="/">CONNECT</Link>
+      </h1>
+      <Link to="/about" className={styles.about}>
+        about
+      </Link>
+      <Link to="/donate" className={styles.donate}>
+        donate
+      </Link>
+      <Link to="/get-app" className={styles.getApp}>
+        get the app
+      </Link>
+      <Link to="/account" className={styles.account}>
+        account
+      </Link>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
