6da6f96797e702cb6e6d949a9b3c627cce14dd71
Matt Eva <matteweva@gmail.com>
2024-02-06 19:02:58 -0800

    created utils file for NewConnectionCard

--- client/src/components/NewConnectionCard/NewConnectionCard.jsx
+++ client/src/components/NewConnectionCard/NewConnectionCard.jsx
@@ -1,77 +1,61 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
-import CardImageIcon from "../CardImageIcon/CardImageIcon"
-import styles from "./NewConnectionCard.module.css"
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-function NewConnectionCard({name, uId, pending, invited, profileImg}) {
-    const [pendingInvite, setPendingInvite] = useState(pending)
-    const [connected, setConnected] = useState(false)
+import CardImageIcon from "../CardImageIcon/CardImageIcon";
 
-    const addConnection = async () =>{
-        try {
-            const res = await fetch("/api/invite-connection", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                credentials: "include",
-                body: JSON.stringify({connectionId: uId})
-            })
-            if(res.ok){
-                setPendingInvite(true)
-            } else{
-                const error = await res.json()
-                console.log(error)
-            }
-        } catch(e){
-            console.error(e)
-        }
-    }
+import styles from "./NewConnectionCard.module.css";
 
-    const acceptInvitation = async () => {
-        try {
-            const res = await fetch("/api/accept-invitation", {
-                method: "POST",
-                credentials: "include",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({connectionId: uId})
-            })
-            if (res.ok){
-                setConnected(true)
-            }
-        } catch (e) {
-            console.error(e)
-        }
-    }
+import { addConnection, acceptInvitation } from "./UtilsNewConnectionCard";
 
-    const iconUser = [{
-        name,
-        profileImg,
-        uId
-    }]
+function NewConnectionCard({ name, uId, pending, invited, profileImg }) {
+  const [pendingInvite, setPendingInvite] = useState(pending);
+  const [connected, setConnected] = useState(false);
 
-    return (
-        <article className={styles.card}>
-            <div className={styles.imageContainer}>
-                <CardImageIcon users={iconUser} />
-            </div>
-            <span className={styles.name} title={name}>{name}</span>
-            { pendingInvite ? <span> Invitation Pending</span> : 
-                (invited ? 
-                    (connected ? 
-                        <span className={styles.connected}> Connected</span>
-                        : 
-                        <button onClick={acceptInvitation} className={styles.button}> Accept Invitation</button>
-                    )
-                    :
-                    <button onClick={addConnection} className={styles.button}>connect</button>
-                )
-            }
-            <Link to={`/profile/${uId}`} className={styles.link}>view profile</Link>
-        </article>
-      )
+  const handleAddConnection = () => {
+    addConnection({ uId, setPendingInvite });
+  };
+
+  const handleAcceptInvitation = () => {
+    acceptInvitation({ uId, setConnected });
+  };
+
+  const iconUser = [
+    {
+      name,
+      profileImg,
+      uId,
+    },
+  ];
+
+  return (
+    <article className={styles.card}>
+      <div className={styles.imageContainer}>
+        <CardImageIcon users={iconUser} />
+      </div>
+      <span className={styles.name} title={name}>
+        {name}
+      </span>
+      {pendingInvite ? (
+        <span> Invitation Pending</span>
+      ) : invited ? (
+        connected ? (
+          <span className={styles.connected}> Connected</span>
+        ) : (
+          <button onClick={handleAcceptInvitation} className={styles.button}>
+            {" "}
+            Accept Invitation
+          </button>
+        )
+      ) : (
+        <button onClick={handleAddConnection} className={styles.button}>
+          connect
+        </button>
+      )}
+      <Link to={`/profile/${uId}`} className={styles.link}>
+        view profile
+      </Link>
+    </article>
+  );
 }
 
-export default NewConnectionCard
+export default NewConnectionCard;
---
+++ client/src/components/NewConnectionCard/UtilsNewConnectionCard.js
@@ -0,0 +1,40 @@
+const addConnection = async ({ uId, setPendingInvite }) => {
+  try {
+    const res = await fetch("/api/invite-connection", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({ connectionId: uId }),
+    });
+    if (res.ok) {
+      setPendingInvite(true);
+    } else {
+      const error = await res.json();
+      console.log(error);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const acceptInvitation = async ({ uId, setConnected }) => {
+  try {
+    const res = await fetch("/api/accept-invitation", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ connectionId: uId }),
+    });
+    if (res.ok) {
+      setConnected(true);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export { addConnection, acceptInvitation };
