18ca507e93d72b2a59f17f79e4d62305cbe86a93
Matt Eva <matteweva@gmail.com>
2024-02-10 15:06:32 -0800

    updated state path files in appConfig

--- server/src/config/appConfig.ts
+++ server/src/config/appConfig.ts
@@ -10,7 +10,7 @@ app.use(sessionMiddleware);
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../../client/dist")));
+app.use(express.static(path.join(__dirname, "../../../client/dist")));
 
 app.use("/api", router);
 
