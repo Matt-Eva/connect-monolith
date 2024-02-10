fe52613c2a398287dc036d9fb2c9715e950b3323
Matt Eva <matteweva@gmail.com>
2024-02-09 17:16:23 -0800

    fixed typescript errors in create chat user card

--- client/src/components/CreateChatUserCard/CreateChatUserCard.tsx
+++ client/src/components/CreateChatUserCard/CreateChatUserCard.tsx
@@ -1,9 +1,19 @@
 import CardImageIcon from "../CardImageIcon/CardImageIcon";
+
+import { Connection } from "../../types/connection";
+
 import styles from "./CreateChatUserCard.module.css";
 
-function CreateChatUserCard({ user, addParticipant }) {
+function CreateChatUserCard({
+  user,
+  addParticipant,
+}: {
+  user: Connection;
+  addParticipant: Function;
+}) {
   const iconUser = [
     {
+      name: user.name,
       firstName: user.name,
       profileImg: user.profileImg,
       uId: user.uId,
