d5b38698fdf318a6b9124b6bbd3ac828e27078cd
Matt Eva <matteweva@gmail.com>
2024-02-13 17:03:13 -0800

    installed cassandra driver along with typescript Astra driver

---
+++ server/src/config/cassandraConfig.ts
@@ -0,0 +1,6 @@
+import { AstraDB } from "@datastax/astra-db-ts";
+
+const astraDb = new AstraDB(
+  process.env.CASSANDRA_TOKEN,
+  "https://6e6d83d9-66c9-46f0-b043-3dda06cd3149-us-east1.apps.astra.datastax.com",
+);
