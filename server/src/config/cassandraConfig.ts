6a51642fc0cd4d0e217a5b23d728a1fc28cc8142
Matt Eva <matteweva@gmail.com>
2024-02-13 19:12:09 -0800

    troubleshooting astra connectivity issue

--- server/src/config/cassandraConfig.ts
+++ server/src/config/cassandraConfig.ts
@@ -5,4 +5,15 @@ const astraClient = new AstraDB(
   "https://6e6d83d9-66c9-46f0-b043-3dda06cd3149-us-east1.apps.astra.datastax.com",
 );
 
+const verifyConnectivity = async () => {
+  try {
+    await astraClient.connect();
+    console.log("astra connection successful");
+  } catch (error) {
+    console.error("astra connection error", error);
+  }
+};
+
+verifyConnectivity();
+
 export default astraClient;
